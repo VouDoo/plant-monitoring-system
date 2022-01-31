2837ae0bf575c52d8c82270c47a9ea53b3208147
Maxence <maxgrymonprez@live.fr>
2022-01-31 17:39:45 +0000

    Improved capture_to_file Python script

--- code/raspberry_pi/capture_to_file.py
+++ code/raspberry_pi/capture_to_file.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 """Capture to file
 
 This script captures photos taken by the Raspberry Pi camera module and save them as files.
@@ -6,28 +8,31 @@ It captures a photo each time the button is pressed.
 Before using this script, ensure that the /home/pi/camera directory exists.
 """
 
-import RPi.GPIO as GPIO
 from time import sleep, strftime
+# GPIO module(s)
+import RPi.GPIO as GPIO
+# camera module(s)
 from picamera import PiCamera
 
-CAMBTN = 23  # pin 16
-CAMLED = 32
 
+# GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
+# camera
+CAMBTN = 23  # pin 16
+CAMLED = 32  # camera integreted pin
 GPIO.setup(CAMLED, GPIO.OUT, initial=False)
 GPIO.setup(CAMBTN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
 camera = PiCamera()
 
 while True:
-    GPIO.output(CAMLED, False)
+    GPIO.output(CAMLED, False)  # turn off camera led
     if GPIO.input(CAMBTN) == False:
         print("Capturing photo...")
         fpath = "/home/pi/camera/{}.jpg".format(strftime("%Y%m%d-%H%M%S"))
-        GPIO.output(CAMLED, True)
+        GPIO.output(CAMLED, True)  # turn on camera led
         camera.start_preview()
         sleep(2)  # Camera warm-up time
         camera.capture(fpath, "jpeg")
         camera.stop_preview()
-        print(f"Saved as {fpath}.")
+        print(f"Saved as \"{fpath}\".")
