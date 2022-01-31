32f8c7563337a4a9fbe1d3ed1a30d3ecc98d40ea
Maxence <maxgrymonprez@live.fr>
2022-01-31 18:48:11 +0000

    Create get_lux_metrics.py

---
+++ code/raspberry_pi/get_lux_metrics.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+"""Get lux metrics
+
+This script gets lux and light metrics from the sensor.
+"""
+
+from time import sleep
+# lux sensor module(s)
+from board import SDA, SCL
+from busio import I2C
+import adafruit_veml7700
+
+
+# lux sensor
+lux_sensor = adafruit_veml7700.VEML7700(I2C(SCL, SDA))
+
+while True:
+    print(f"Lux sensor: ambient light={lux_sensor.light}, lux={lux_sensor.lux}")
+    sleep(1)
