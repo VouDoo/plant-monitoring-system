8ead3af811a8cad81bbb735d77716770cb8874b3
VouDoo <maxgrymonprez@live.fr>
2022-03-04 17:46:46 +0000

    Add threadpool exceptions handler

--- code/rpi/main/__main__.py
+++ code/rpi/main/__main__.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
+import os
 import logging
 import queue
 import threading
-import concurrent.futures
+from concurrent.futures import ThreadPoolExecutor
 
 # internal module(s)
 import config
@@ -14,7 +15,6 @@ import lux_sensor
 import camera
 import exporter
 
-
 if __name__ == "__main__":
     logging.basicConfig(
         format="%(asctime)s - %(levelname)s - %(message)s",
@@ -23,6 +23,8 @@ if __name__ == "__main__":
         encoding="utf-8",
         filename=config.LOG_FILE,
     )
+
+    logging.info("Starting Plant Monitoring System.")
 
     gpio.init()
 
@@ -33,11 +35,30 @@ if __name__ == "__main__":
     lux_sensor = lux_sensor.LuxSensor()
     camera = camera.Camera()
 
-    logging.info("Starting Plant Monitoring System.")
+    futures = {}
 
-    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
-        executor.submit(tasks.executor, tasks_queue, event, led_strip, camera)
-        executor.submit(tasks.led_strip_handler, tasks_queue, event, led_strip)
-        executor.submit(tasks.lux_sensor_handler, tasks_queue, event, lux_sensor, led_strip)
-        executor.submit(tasks.camera_handler, tasks_queue, event, camera)
-        executor.submit(exporter.serve, event, led_strip, lux_sensor)
+    with ThreadPoolExecutor() as executor:
+        futures["tasks_executor"] = executor.submit(
+            tasks.executor, tasks_queue, event, led_strip, camera
+        )
+        futures["tasks_led_strip_handler"] = executor.submit(
+            tasks.led_strip_handler, tasks_queue, event, led_strip
+        )
+        futures["tasks_lux_sensor_handler"] = executor.submit(
+            tasks.lux_sensor_handler, tasks_queue, event, lux_sensor, led_strip
+        )
+        futures["tasks_camera_handler"] = executor.submit(
+            tasks.camera_handler, tasks_queue, event, camera
+        )
+        futures["exporter_serve"] = executor.submit(
+            exporter.serve, event, led_strip, lux_sensor
+        )
+
+        while not event.is_set():
+            event.wait(1)
+            for name, call in futures.items():
+                if not call.running():
+                    logging.error(f'Thread "{name}" has stopped: {call.exception()}.')
+                    event.set()
+                    executor.shutdown(wait=True, cancel_futures=True)
+                    os._exit(1)
--- code/rpi/main/tasks.py
+++ code/rpi/main/tasks.py
@@ -29,10 +29,10 @@ def led_strip_handler(
     while not event.is_set():
         if led_strip.is_button_pressed():
             if led_strip.is_force_off_enabled():
-                logging.info('LED button pressed, disable force off.')
+                logging.info("LED button pressed, disable force off.")
                 led_strip.enable_force_off(False)
             else:
-                logging.info('LED button pressed, enable force off.')
+                logging.info("LED button pressed, enable force off.")
                 led_strip.enable_force_off(True)
                 tasks_queue.put(tasks_module.Tasks.TURN_OFF_LED)
         event.wait(1)
@@ -98,20 +98,22 @@ def executor(
     camera: camera_module.Camera,
 ) -> None:
     """Consume and execute requested tasks from queue."""
-    while not event.is_set() or not tasks_queue.empty():
-        todo = tasks_queue.get()
-        if todo == Tasks.TURN_OFF_LED:
-            logging.info('Execute "Turn off LED" task.')
-            led_strip.set_mode(led_strip_module.LedMode.OFF)
-        elif todo == Tasks.SET_LED_FOR_PLANT:
-            logging.info('Execute "Switch LED to PLANT mode" task.')
-            led_strip.set_mode(led_strip_module.LedMode.PLANT)
-        elif todo == Tasks.TAKE_PICTURE:
-            logging.info('Execute "Take picture" task.')
-            if not led_strip.is_force_off_enabled():
-                led_strip.set_mode(led_strip_module.LedMode.CAMERA, False)
-            camera.capture()
-            if not led_strip.is_force_off_enabled():
-                led_strip.set_mode(led_strip.get_currmode())
-        else:
-            logging.error(f'Cannot execute task bacause "{todo.name}" task is unknown.')
+    while not event.is_set():
+        if not tasks_queue.empty():
+            todo = tasks_queue.get(False, 10)
+            if todo == Tasks.TURN_OFF_LED:
+                logging.info('Execute "Turn off LED" task.')
+                led_strip.set_mode(led_strip_module.LedMode.OFF)
+            elif todo == Tasks.SET_LED_FOR_PLANT:
+                logging.info('Execute "Switch LED to PLANT mode" task.')
+                led_strip.set_mode(led_strip_module.LedMode.PLANT)
+            elif todo == Tasks.TAKE_PICTURE:
+                logging.info('Execute "Take picture" task.')
+                if not led_strip.is_force_off_enabled():
+                    led_strip.set_mode(led_strip_module.LedMode.CAMERA, False)
+                camera.capture()
+                if not led_strip.is_force_off_enabled():
+                    led_strip.set_mode(led_strip.get_currmode())
+            else:
+                logging.error(f'Cannot execute task because "{todo.name}" is unknown.')
+        event.wait(1)
